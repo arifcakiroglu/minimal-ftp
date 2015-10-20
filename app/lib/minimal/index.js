8c784d3afd66807563043ef892dcf6927d84fd1e
arifcakiroglu <cakirogluarif@gmail.com>
2015-10-20 21:32:26 +0300

    switch account

--- app/lib/minimal/index.js
+++ app/lib/minimal/index.js
@@ -19,25 +19,42 @@ module.exports = {
       setTimeout(function(){
         $(".message").slideUp()
       }, 3000);
-      console.log("Succsess")
-      console.dir(list)
 
+
+      list.forEach(function(e){
+        //console.dir(e)
+      })
 
 
     });
+
+
+this.client.status(function(err, currentDir){
+  if (err) throw err;
+  console.log(currentDir)
+});
+
+    this.client.lastMod("/htdocs/1.png",function(err, currentDir){
+      if (err) throw err;
+      //console.log(currentDir)
+    });
+
 
     this.client.cwd("/htdocs/stylesheets",function(err, currentDir){
       if (err) throw err;
-      console.log(currentDir)
+      //console.log(currentDir)
     });
 
-  
+
 
     this.client.pwd(function(err, currentDir){
       if (err) throw err;
-      console.log(currentDir)
+      //console.log(currentDir)
     });
-
+    this.client.listSafe(function(err, currentDir){
+      if (err) throw err;
+      //console.dir(currentDir)
+    });
 
   },
   ready: function(){
